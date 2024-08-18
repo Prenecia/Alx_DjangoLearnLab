9751d69b1c9e393f49183fd0ba3cbfca8f4ccc7d
Prenecia <michaelaprenecia0@gmail.com>
2024-08-18 13:30:30 -0700

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from .models import Book, Library, UserProfile
+["from django.contrib.auth.decorators import permission_required"]
 
 # Authentication Views
 def register_view(request):
