bfcda1de758858d45f4881ee9154f681a45aa0ba
Prenecia <michaelaprenecia0@gmail.com>
2024-08-18 11:47:00 -0700

    update

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -16,3 +16,34 @@ class LibraryDetailView(DetailView):
 ["relationship_app/list_books.html"]
 ["relationship_app/library_detail.html"]
 ["from django.views.generic.detail import DetailView"]
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
+    return render(request, 'logout.html')
