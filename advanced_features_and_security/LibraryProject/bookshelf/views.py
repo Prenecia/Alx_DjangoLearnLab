c8ee3358d9c149ace15e807458becef9d941f17a
Prenecia <michaelaprenecia0@gmail.com>
2024-08-30 07:57:44 -0700

    update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from .models import Book
+from django.http import HttpResponse
 
 @permission_required('yourapp.can_view', raise_exception=True)
 def book_detail_view(request, pk):
@@ -33,3 +34,13 @@ def book_delete_view(request, pk):
     return render(request, 'book_confirm_delete.html', {'book': book})
 
 books
+
+def search_books(request):
+    query = request.GET.get('q')
+    books = Book.objects.filter(title__icontains=query)
+    return render(request, 'book_list.html', {'books': books})
+
+def my_view(request):
+    response = HttpResponse('Hello, world!')
+    response['Content-Security-Policy'] = "default-src 'self';"
+    return response
