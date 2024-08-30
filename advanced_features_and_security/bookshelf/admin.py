d26c14c4feba991a9e1728edc28a2698f23c94c9
Prenecia <michaelaprenecia0@gmail.com>
2024-08-30 06:41:51 -0700

    update

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
