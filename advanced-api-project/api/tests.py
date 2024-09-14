d56f087f40a4b5d013d0b3ddd8ddb323fd727756
Prenecia <michaelaprenecia0@gmail.com>
2024-09-14 13:40:18 -0700

    update

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Book, Author
 
-# Create your tests here.
+class BookTests(APITestCase):
+    def test_create_book(self):
+        url = reverse('book-list')
+        data = {'title': 'Test Book', 'publication_year': 2023, 'author': 1}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
