c4640733074be40b49ab6db5045acb3d4695c5ae
Prenecia <michaelaprenecia0@gmail.com>
2024-09-14 14:27:55 -0700

    update

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
