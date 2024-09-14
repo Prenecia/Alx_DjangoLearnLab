55cd2a53511fff0f8b4d308b7cb27eb1469d706e
Prenecia <michaelaprenecia0@gmail.com>
2024-09-14 14:29:55 -0700

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,3 +16,5 @@ urlpatterns += [
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
+
+["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"]
