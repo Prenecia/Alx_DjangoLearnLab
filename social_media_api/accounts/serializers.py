1214fe72de3942f3a4cdc5cce79f4ff438267cd4
Prenecia <michaelaprenecia0@gmail.com>
2024-09-25 08:27:06 -0700

    update

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'bio', 'profile_picture']
+
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ['key', 'user']
